962014f5526595ce35dcb7dadf387c5690563be3
EC2 Default User <ec2-user@ip-172-31-32-168.us-west-2.compute.internal>
2015-05-06 04:52:20 +0000

    update

--- app.js
+++ app.js
@@ -1,50 +1,13 @@
 var app = require('express.io')()
-var net = require('net');
 var fs = require('fs');
-var telnet = require('./lib/telnet.js');
+var ptt = require('./lib/ptt.js');
 
-	var client = new net.Socket();
-	client.connect('23', 'ptt.cc', function() {
-		console.log('CONNECTED TO: ptt.cc:23');
-		var mycmd = [[251,24,0,'VT100'],[251,31,80,24],[253,1],[253,3],[254,0],[252,0]];
-		var data = telnet.cmd2data(mycmd);
-		client.write(data);
-		//console.log('mycmd:  ' + mycmd.toString());
-		//var ret = telnet.parsing(data);
-		//console.log('mycmd2: ' + ret.cmd.toString());
-	});
-var logined = false;
-var accountset = false;
-	client.on('data', function(data){
-		var ret = telnet.parsing(data);
-		//console.log('DATA: ' + data);
-		//console.log('cmd: ' + ret.cmd.toString());
-		if (ret.data.length > 0) {
-			console.log(ret.data.toString());
-			if (!logined) {
-				if (ret.data[ret.data.length-1] == 0x08) {
-					fs.readFile('acc','utf8',function(err,data){
-						for (i=0; i<data.length; i++) {
-							client.write(data[i]);
-						}
-						client.write('\r');
-						accountset = true;
-					});
-				}
-				if (accountset && ret.data[ret.data.length-1] == 0x20) {
-					fs.readFile('pass','utf8',function(err,data){
-						for (i=0; i<data.length; i++) {
-							client.write(data[i]);
-						}
-						client.write('\r');
-					});
-				}
-			}
-		}
-	});
-	client.on('close', function(){
-		console.log('closed\n');
-	});
+var user = fs.readFileSync('user','utf8');
+var pass = fs.readFileSync('pass','utf8');
+
+if (user !== undefined && pass !== undefined) {
+	ptt.login (user,pass);
+}
 
 app.http().io()
 
