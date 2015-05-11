3c47fefca5f8ab2a27c988d06a7d58f4792ef4e0
u8906250 <u8906250@gmail.com>
2015-05-11 17:17:31 +0800

    update

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 var app = require('express.io')()
-var fs = require('fs');
 var ptt = require('./lib/ptt.js');
 
-var user = fs.readFileSync('user','utf8').trim();
-var pass = fs.readFileSync('pass','utf8').trim();
-
-if (user !== undefined && pass !== undefined) {
-	ptt.login (user,pass);
-}
+var client = {"user":'',"pass":'',"status":0};
 
 app.http().io()
 
 app.io.route('ready', function(req) {
 	req.io.emit('talk', {
 		message: 'io event from an io route on the server'
-	})
-})
+	});
+});
+
+app.io.route('client_status', function(req) {
+	req.io.emit('talk', {client_status: client.status});
+});
+
+app.io.route('login', function(req) {
+	if (client.status == 0) {
+		client.user = req.data.user;
+		client.pass = req.data.pass;
+		ptt.login (client);
+		req.io.emit('talk', {client_status: client.status});
+	}
+});
+
 
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/views/client.html')
